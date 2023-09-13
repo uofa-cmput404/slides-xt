3ce41bab49a1a42927a1e8728cbb84b80b2d0ae3
Hazel Victoria Campbell <hazel.campbell@ualberta.ca>
2023-09-13 12:49:22 -0600

    npm_run_deploy

--- main.js
+++ main.js
@@ -749,6 +749,7 @@ table.wikitable td, table.wikitable th {
 body {
     --r-main-font: 'Lexend Deca Variable', 'Source Sans Pro', 'Helvetica', sans-serif;
     --r-heading-font: 'Lexend Deca Variable', 'Source Sans Pro', 'Helvetica', sans-serif;
+    --r-background-color: #ffc;
 }
 cite {
     font-size: 50%;
