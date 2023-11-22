041f2af0425379b312e20464ba060ca2cec137c5
Hazel Victoria Campbell <hazel.campbell@ualberta.ca>
2023-11-22 10:43:34 -0700

    unregister serviceworkers

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import Reveal from "reveal.js";
 import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((workers) => {
+    for (worker of workers) {
+      worker.unregister();
+    }
+  });
+}
+
 Reveal.initialize({
     hash: true,
     respondToHashChanges: true,
@@ -9,4 +17,3 @@ Reveal.initialize({
         RevealMarkdown
     ]
 });
-
