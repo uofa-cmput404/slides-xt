db8d1746f05e3231f15cdc346f24fa02e9f873ca
Hazel Victoria Campbell <hazel.campbell@ualberta.ca>
2023-11-23 12:48:48 -0700

    try vite beta

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import {prexample} from './src/scripts/prexample.js'
 export default {
     // base: 'https://uofa-cmput404.github.io/slides-xt/',
     base: '/slides-xt/',
+    // base: './',
     build: {
         minify: false,
         rollupOptions: {
